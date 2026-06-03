1bfdca40a3f5ea422f8177e13aadf5fc6317befc
Samukelo khathi <145541253+Samukelokhathi@users.noreply.github.com>
2026-06-03 13:35:24 +0200

    getting elements by id

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-console.log("Hello World")
+let signUpPortal = document.getElementById("signUpPortal");
+let username = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let submitBtn = document.getElementById("submit-btn")
 
-console.log('testing main branch')
