fdb76a9b1cea78e0e2de6c507c99b2e712c27680
Samukelo khathi <145541253+Samukelokhathi@users.noreply.github.com>
2026-06-06 12:50:16 +0200

    Added studentDatabase Array to main js file

--- main.js
+++ main.js
@@ -91,6 +91,15 @@ function verificationBtn() {
 
     btnContainer.append(resendBtn, countDownSpan)
     document.body.appendChild(btnContainer)
-
-
 }
+
+
+const studentDatabase = [
+    { "first-name": "Bayanda", "last-name": "Zuma", age: 18, email: "zuma123@gmail.com", "course": "Cyber Security" },
+    { "first-name": "Sanele", "last-name": "Mbatha", age: 20, email: "mbata123@gmail.com", "course": "Administrator" },
+    { "first-name": "Zanele", "last-name": "Mthembu", age: 30, email: "mazet23@gmail.com", "course": "Medicine" },
+    { "first-name": "Snethemba", "last-name": "Goqo", age: 26, email: "sne@gmail.com", "course": "Law" },
+    { "first-name": "Neliswa", "last-name": "Ngobese", age: 21, email: "mangobese90@gmail.com", "course": "Software Development" },
+]
+
+// console.log(studentDatabase[0])
